27240d28fb5a97f56d1c7c2c1095d797ca6bb6ea
Cody Dutremble-Collins <codster672@gmail.com>
2025-12-08 22:06:29 -0500

    Added coin UI and Different coin amounts

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,14 +1,35 @@
 using UnityEngine;
+using TMPro;
 
 public class Coin : MonoBehaviour
 {
+    private TMP_Text coinText;
+    public int coinsToGive = 1;
+
+    private void Start()
+    {
+        // Automatically find ANY TMP_Text in the scene
+        // OR use the specific name if needed
+        coinText = GameObject.Find("CoinText")?.GetComponent<TMP_Text>();
+
+        
+        
+            Debug.Log("Found CoinText? " + (coinText != null));
+        
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.CompareTag("Player"))
         {
             SoundManager.Instance.PlaySFX("COIN", 0.4f);
-            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-            player.coins += 1;
+
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.coins += coinsToGive;
+
+            if (coinText != null)
+                coinText.text = player.coins.ToString();
+
             Destroy(gameObject);
         }
     }
